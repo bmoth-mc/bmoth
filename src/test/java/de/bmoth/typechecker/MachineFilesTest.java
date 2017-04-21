d09c86431d215e7ef22545ad77c2f14d7f6b18b7
dohan <dohan001@hhu.de>
2017-04-21 15:45:40 +0200

    completed test testReadSimpleMachineFromFile

--- src/test/java/de/bmoth/typechecker/MachineFilesTest.java
+++ src/test/java/de/bmoth/typechecker/MachineFilesTest.java
@@ -2,17 +2,27 @@ package de.bmoth.typechecker;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import de.bmoth.parser.Parser;
+import de.bmoth.parser.ast.nodes.DeclarationNode;
 import de.bmoth.parser.ast.nodes.MachineNode;
 
 public class MachineFilesTest {
 
 	@Test
-	public void testSimpleMachine() throws Exception {
+	public void testReadSimpleMachineFromFile() throws Exception {
 		String dir = "src/test/resources/machines/";
 		MachineNode machineNode = Parser.getMachineFileAsSemanticAst(dir + "SimpleMachine.mch");
-		//TODO complete
+		List<DeclarationNode> variables = machineNode.getVariables();
+		assertEquals(1, variables.size());
+		assertEquals("x", variables.get(0).getName());
+		assertEquals("INTEGER", variables.get(0).getType().toString());
+		List<DeclarationNode> constants = machineNode.getConstants();
+		assertEquals(1, constants.size());
+		assertEquals("con", constants.get(0).getName());
+		assertEquals("BOOL", constants.get(0).getType().toString());
 	}
 }
