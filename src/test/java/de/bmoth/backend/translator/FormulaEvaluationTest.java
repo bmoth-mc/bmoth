bb5bcfc0e2a2e60b51087153003ebd9ef5a930f4
xmoex <moritz.gericke@uni-duesseldorf.de>
2017-04-26 21:10:35 +0200

    added first test-case for computation of all solutions for a given predicate
    contributing to #10

--- src/test/java/de/bmoth/backend/translator/FormulaEvaluationTest.java
+++ src/test/java/de/bmoth/backend/translator/FormulaEvaluationTest.java
@@ -11,10 +11,14 @@ import org.junit.Test;
 import com.microsoft.z3.BoolExpr;
 import com.microsoft.z3.Context;
 import com.microsoft.z3.Expr;
+import com.microsoft.z3.IntNum;
 import com.microsoft.z3.Solver;
 import com.microsoft.z3.Status;
 
 import de.bmoth.backend.FormulaTranslator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FormulaEvaluationTest {
 
@@ -180,5 +184,33 @@ public class FormulaEvaluationTest {
         Status check = s.check();
 
         assertEquals(Status.SATISFIABLE, check);
+    }
+
+    @Test
+    public void testAllSolutions() throws Exception {
+        String formula = "1 < x & x < 5";
+        // getting the translated z3 representation of the formula
+        BoolExpr constraint = FormulaTranslator.translatePredicate(formula, ctx);
+
+        s.add(constraint);
+
+        Expr x = ctx.mkIntConst("x");
+
+        // 1st try: brute force over 'all' satisfying solutions
+        // credit goes to: http://stackoverflow.com/questions/13395391/z3-finding-all-satisfying-models#answer-13398853
+
+        List<Number> solutions = new ArrayList<>();
+
+        // as long as formula is satisfiable
+        for (int i = 0; s.check() == Status.SATISFIABLE && i < 10; i++) {
+            // get current evaluation for x
+            IntNum currentX = (IntNum) s.getModel().eval(x, true);
+            // and exclude it from formula
+            s.add(ctx.mkNot(ctx.mkEq(x, currentX)));
+            // store result
+            solutions.add(currentX.getInt());
+        }
+
+        assertEquals("[2, 3, 4]", solutions.toString());
     }
 }
