a235692bed061cae5b9fb80b2b553ad6b38d8978
Sebastian Krings <sebastian@krin.gs>
2017-04-26 10:19:09 +0200

    replace command line interface by simple gui

--- src/main/java/de/bmoth/App.java
+++ src/main/java/de/bmoth/App.java
@@ -1,10 +1,76 @@
 package de.bmoth;
-public class App {
-	public String getGreeting() {
-		return "Hello world.";
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import org.fxmisc.flowless.VirtualizedScrollPane;
+import org.fxmisc.richtext.CodeArea;
+import org.fxmisc.richtext.LineNumberFactory;
+import org.fxmisc.richtext.model.StyleSpans;
+import org.fxmisc.richtext.model.StyleSpansBuilder;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.regex.Matcher;
+
+public class App extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+        MenuBar menuBar = new MenuBar();
+        Menu menuFile = new Menu("File");
+        Menu menuCheck = new Menu("Checks");
+
+        MenuItem open = new MenuItem("Open");
+        open.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent t) {
+                openFile();
+            }
+        });
+
+        MenuItem exit = new MenuItem("Exit");
+        exit.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent t) {
+                System.exit(0);
+            }
+        });
+
+        menuFile.getItems().addAll(open,new SeparatorMenuItem(),exit);
+        menuBar.getMenus().addAll(menuFile, menuCheck);
+
+        CodeArea codeArea = new CodeArea();
+		codeArea.setParagraphGraphicFactory(LineNumberFactory.get(codeArea));
+
+		codeArea.richChanges()
+				.filter(ch -> !ch.getInserted().equals(ch.getRemoved())) // XXX
+				.subscribe(change -> {
+					codeArea.setStyleSpans(0, computeHighlighting(codeArea.getText()));
+				});
+
+		Scene scene = new Scene(new VBox(),800,600);
+
+        ((VBox) scene.getRoot()).getChildren().addAll(menuBar,codeArea);
+
+        primaryStage.setScene(scene);
+		primaryStage.setTitle("BMoth");
+		primaryStage.show();
 	}
 
-	public static void main(String[] args) {
-		System.out.println(new App().getGreeting());
-	}
+    private static StyleSpans<Collection<String>> computeHighlighting(String text) {
+        StyleSpansBuilder<Collection<String>> spansBuilder
+                = new StyleSpansBuilder<>();
+        spansBuilder.add(Collections.emptyList(), text.length());
+        return spansBuilder.create();
+    }
+
+    private static void openFile() {
+
+    }
 }
