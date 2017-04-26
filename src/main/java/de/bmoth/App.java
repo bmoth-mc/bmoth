4dd91d86de41fb9144156682de86aa5fd774fb73
Julian <Julian.Ullrich@uni-duesseldorf.de>
2017-04-26 15:53:38 +0200

    Read File

--- src/main/java/de/bmoth/App.java
+++ src/main/java/de/bmoth/App.java
@@ -1,10 +1,14 @@
 package de.bmoth;
 
-import java.io.File;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.Collections;
 
 import javafx.stage.FileChooser;
+import org.fxmisc.flowless.VirtualizedScrollPane;
 import org.fxmisc.richtext.CodeArea;
 import org.fxmisc.richtext.LineNumberFactory;
 import org.fxmisc.richtext.model.StyleSpans;
@@ -44,16 +48,31 @@ public class App extends Application {
             }
         });
 
-        menuFile.getItems().addAll(open, new SeparatorMenuItem(), exit);
+        MenuItem save = new MenuItem("Save");
+        save.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                saveFile();
+            }
+        });
+
+        MenuItem saveAs = new MenuItem("Save As");
+        saveAs.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                saveFileAS();
+            }
+        });
+
+        menuFile.getItems().addAll(open,save,saveAs, new SeparatorMenuItem(), exit);
         menuBar.getMenus().addAll(menuFile, menuCheck);
 
         codeArea.setParagraphGraphicFactory(LineNumberFactory.get(codeArea));
-
         codeArea.richChanges().filter(ch -> !ch.getInserted().equals(ch.getRemoved())) // XXX
                 .subscribe(change -> {
                     codeArea.setStyleSpans(0, computeHighlighting(codeArea.getText()));
                 });
-
+        codeArea.setPrefSize(800,600);
         Scene scene = new Scene(new VBox(), 800, 600);
 
         ((VBox) scene.getRoot()).getChildren().addAll(menuBar, codeArea);
@@ -63,21 +82,38 @@ public class App extends Application {
         primaryStage.show();
     }
 
+
+
     private static StyleSpans<Collection<String>> computeHighlighting(String text) {
         StyleSpansBuilder<Collection<String>> spansBuilder = new StyleSpansBuilder<>();
         spansBuilder.add(Collections.emptyList(), text.length());
         return spansBuilder.create();
     }
 
-    private static void openFile(Stage stage,CodeArea codeArea) {
+    private static void openFile(Stage stage,CodeArea codeArea)  {
         FileChooser fileChooser= new FileChooser();
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Open MCH File", "*.mch"));
         fileChooser.setTitle("Choose File");
         fileChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
         File file=fileChooser.showOpenDialog(stage);
+
         if(file!=null){
-            codeArea.appendText(file.getAbsolutePath());
+            String content = null;
+            try {
+                content = new String(Files.readAllBytes(Paths.get(file.getPath())));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try{codeArea.clear();}catch (Exception e){
+                e.printStackTrace();
+            }
+            codeArea.appendText(content);
         }
+    }
+    private void saveFileAS() {
+    }
+
+    private void saveFile() {
     }
 
     public static void main(String[] args){
