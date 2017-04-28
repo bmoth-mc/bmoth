4880f00bf5af3f12d7366980b2204daab051e61c
xmoex <moritz.gericke@uni-duesseldorf.de>
2017-04-28 20:19:41 +0200

    enable State to generate a combined BoolExpr of it's stored values

--- src/main/java/de/bmoth/modelchecker/State.java
+++ src/main/java/de/bmoth/modelchecker/State.java
@@ -1,5 +1,7 @@
 package de.bmoth.modelchecker;
 
+import com.microsoft.z3.BoolExpr;
+import com.microsoft.z3.Context;
 import com.microsoft.z3.Expr;
 
 import java.util.Map;
@@ -15,5 +17,19 @@ public class State {
     public State(State predecessor, Map<String, Expr> values) {
         this.predecessor = predecessor;
         this.values = values;
+    }
+
+    public BoolExpr getValuesExpression(Context context) {
+        BoolExpr result = null;
+        for (Map.Entry<String, Expr> entry : values.entrySet()) {
+            BoolExpr singleExpression = (BoolExpr) entry.getValue();
+
+            if (result == null) {
+                result = singleExpression;
+            } else {
+                result = context.mkAnd(result, singleExpression);
+            }
+        }
+        return result;
     }
 }
