391ad8e0590384cc53b06d38b8f9cfd69ac18a3b
xmoex <moritz.gericke@uni-duesseldorf.de>
2017-05-15 23:04:59 +0200

    extracted minimal example: combination of x² = x*x and #x.(x:INTEGER)#x.(x:INTEGER & {x} \\/ {1,2} = {1,2} ) produces Status.UNKNOWN after sat-check of invariant
    contributing to #59

--- src/test/java/de/bmoth/modelchecker/ModelCheckerTest.java
+++ src/test/java/de/bmoth/modelchecker/ModelCheckerTest.java
@@ -2,6 +2,7 @@ package de.bmoth.modelchecker;
 
 import de.bmoth.parser.Parser;
 import de.bmoth.parser.ast.nodes.MachineNode;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -50,6 +51,24 @@ public class ModelCheckerTest {
         ModelCheckingResult result = ModelChecker.doModelCheck(machine);
         // the operation BlockSubstitution will finally violate the invariant x<=2
         assertEquals(false, result.isCorrect());
+    }
+
+    @Test
+    @Ignore
+    public void testIssue59() throws Exception {
+        String machine = "MACHINE SimpleMachine\n";
+        machine += "VARIABLES x\n";
+        machine += "INVARIANT x : INTEGER &\n";
+        machine += "\tx**2 = x*x &\n";
+        machine += "\t#x.(x:INTEGER & {x} \\/ {1,2} = {1,2} )\n";
+        machine += "INITIALISATION x := -3\n";
+        machine += "OPERATIONS\n";
+        machine += "\tIncX = SELECT x < 50 THEN x := x+1 END\n";
+        machine += "END";
+
+        ModelCheckingResult result = ModelChecker.doModelCheck(machine);
+        // the operation BlockSubstitution will finally violate the invariant x<=2
+        assertEquals(true, result.isCorrect());
     }
 
 }
