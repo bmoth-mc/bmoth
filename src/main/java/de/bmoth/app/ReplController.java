a1c7765e84e5256faa54571f5e4eb342cea962bb
Jessica Petrasch <jessica.petrasch@uni-duesseldorf.de>
2017-05-19 15:17:02 +0200

    Read predicate and find one solution or return UNSATISFIABLE

--- src/main/java/de/bmoth/app/ReplController.java
+++ src/main/java/de/bmoth/app/ReplController.java
@@ -1,7 +1,53 @@
 package de.bmoth.app;
 
-/**
- * Created by jessy on 19.05.17.
- */
-public class ReplController {
+import com.microsoft.z3.BoolExpr;
+import com.microsoft.z3.Context;
+import com.microsoft.z3.Model;
+import com.microsoft.z3.Solver;
+import de.bmoth.backend.FormulaToZ3Translator;
+import de.bmoth.backend.SolutionFinder;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+public class ReplController implements Initializable {
+
+    @FXML
+    TextArea replText = new TextArea();
+
+    private Context ctx;
+    private Solver s;
+
+    @Override
+    public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
+
+        ctx = new Context();
+        s = ctx.mkSolver();
+
+        replText.setOnKeyPressed(keyEvent -> {
+            if (keyEvent.getCode() == KeyCode.ENTER) {
+                String[] predicate = replText.getText().split("\n");
+                String solution = processPredicate(predicate[predicate.length-1]);
+                replText.appendText(solution);
+            }
+        });
+    }
+
+    private String processPredicate(String predicate) {
+        ctx = new Context();
+        s = ctx.mkSolver();
+        BoolExpr constraint = FormulaToZ3Translator.translatePredicate(predicate, ctx);
+        SolutionFinder finder = new SolutionFinder(constraint, s, ctx);
+        Set<Model> solutions = finder.findSolutions(1);
+        if (solutions.size() == 0) {
+            return "\nUNSATISFIABLE";
+        } else {
+            return "\n" + solutions.toString();
+        }
+    }
 }
