372a00672b234c8c684e328b0d03f69faf3d3baf
Sebastian Krings <sebastian@krin.gs>
2017-05-21 16:12:05 +0200

    add base class for headless ui tests

--- src/test/java/de/bmoth/app/HeadlessUITest.java
+++ src/test/java/de/bmoth/app/HeadlessUITest.java
@@ -1,7 +1,20 @@
 package de.bmoth.app;
 
-/**
- * Created by krings on 21.05.17.
- */
-public class HeadlessUITest {
+import org.junit.BeforeClass;
+import org.testfx.framework.junit.ApplicationTest;
+
+import static org.testfx.api.FxToolkit.registerPrimaryStage;
+
+public abstract class HeadlessUITest extends ApplicationTest {
+    @BeforeClass
+    public static void setupHeadlessIfRequested() throws Exception {
+        if (Boolean.getBoolean("headless")) {
+            System.setProperty("testfx.robot", "glass");
+            System.setProperty("testfx.headless", "true");
+            System.setProperty("prism.order", "sw");
+            System.setProperty("prism.text", "t2k");
+            System.setProperty("java.awt.headless", "true");
+        }
+        registerPrimaryStage();
+    }
 }
