7e4076b903e2d6b7c0898c77e5f736591a1055f4
Sebastian Krings <sebastian@krin.gs>
2017-05-22 11:27:17 +0200

    two simple tests for the app controller

---
+++ src/test/java/de/bmoth/app/AppControllerTest.java
@@ -0,0 +1,51 @@
+package de.bmoth.app;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.junit.Test;
+
+import static javafx.scene.input.KeyCode.ENTER;
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.matcher.base.NodeMatchers.hasText;
+
+public class ReplControllerTest extends HeadlessUITest {
+    private String replId = "#replText";
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("repl.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root, 500, 300);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Test
+    public void replTypeSimplePredicateTest() {
+        clickOn(replId).write("x = 5");
+        verifyThat(replId, hasText("x = 5"));
+
+        push(ENTER);
+        verifyThat(replId, hasText("x = 5\n{x=5}\n"));
+    }
+
+    @Test
+    public void replTypeUnsatisfiablePredicateTest() {
+        clickOn(replId).write("x = 5 & x = 6");
+        verifyThat(replId, hasText("x = 5 & x = 6"));
+
+        push(ENTER);
+        verifyThat(replId, hasText("x = 5 & x = 6\nUNSATISFIABLE\n"));
+    }
+
+    @Test
+    public void replTypeSetPredicateTest() {
+        clickOn(replId).write("x = {1}");
+        verifyThat(replId, hasText("x = {1}"));
+
+        push(ENTER);
+        verifyThat(replId, hasText("x = {1}\n{x=[1 -> true, else -> false]}\n"));
+    }
+}
