52e64e7d850bc86181a082824a5a955825c4fc4a
Sebastian Krings <sebastian@krin.gs>
2017-05-22 13:04:20 +0200

    cleanup after ui tests

--- src/test/java/de/bmoth/app/HeadlessUITest.java
+++ src/test/java/de/bmoth/app/HeadlessUITest.java
@@ -1,11 +1,15 @@
 package de.bmoth.app;
 
+import javafx.stage.Stage;
+import org.junit.After;
 import org.junit.BeforeClass;
 import org.testfx.framework.junit.ApplicationTest;
+import org.testfx.util.WaitForAsyncUtils;
 
 import java.util.concurrent.TimeoutException;
 
 import static org.testfx.api.FxToolkit.registerPrimaryStage;
+import static org.testfx.api.FxToolkit.setupStage;
 
 public abstract class HeadlessUITest extends ApplicationTest {
     @BeforeClass
@@ -18,5 +22,11 @@ public abstract class HeadlessUITest extends ApplicationTest {
             System.setProperty("java.awt.headless", "true");
         }
         registerPrimaryStage();
+    }
+
+    @After
+    public void cleanup() throws Exception {
+        WaitForAsyncUtils.waitForFxEvents();
+        setupStage(Stage::close);
     }
 }
