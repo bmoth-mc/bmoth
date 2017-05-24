905b0ce46ae63c5fdaf6a5a422b94c677dc58c03
Sebastian Krings <sebastian@krin.gs>
2017-05-24 20:49:39 +0200

    fix access

--- src/main/java/de/bmoth/app/BMothPreferences.java
+++ src/main/java/de/bmoth/app/BMothPreferences.java
@@ -3,31 +3,31 @@ package de.bmoth.app;
 public class BMothPreferences {
     private static java.util.prefs.Preferences prefs = java.util.prefs.Preferences.userNodeForPackage(BMothPreferences.class);
 
-    static String getStringPreference(StringPreference p) {
+    public static String getStringPreference(StringPreference p) {
         return prefs.get(p.toString(), p.defaultValue);
     }
 
-    static void setStringPreference(StringPreference p, String val) {
+    public static void setStringPreference(StringPreference p, String val) {
         prefs.put(p.toString(), val);
     }
 
-    static boolean getBooleanPreference(BooleanPreference p) {
+    public static boolean getBooleanPreference(BooleanPreference p) {
         return prefs.getBoolean(p.toString(), p.defaultValue);
     }
 
-    static void setBooleanPreference(BooleanPreference p, boolean val) {
+    public static void setBooleanPreference(BooleanPreference p, boolean val) {
         prefs.put(p.toString(), String.valueOf(val));
     }
 
-    static int getIntPreference(IntPreference p) {
+    public static int getIntPreference(IntPreference p) {
         return prefs.getInt(p.toString(), p.defaultValue);
     }
 
-    static void setIntPreference(IntPreference p, String val) {
+    public static void setIntPreference(IntPreference p, String val) {
         prefs.put(p.toString(), val);
     }
 
-    enum StringPreference {
+    public enum StringPreference {
         LAST_FILE(""), LAST_DIR(System.getProperty("user.dir"));
 
         private String defaultValue;
@@ -38,7 +38,7 @@ public class BMothPreferences {
     }
 
 
-    enum IntPreference {
+    public enum IntPreference {
         MIN_INT(-1), MAX_INT(3), MAX_INITIAL_STATE(5), MAX_TRANSITIONS(5), Z3_TIMEOUT(5000);
 
         private int defaultValue;
@@ -48,7 +48,7 @@ public class BMothPreferences {
         }
     }
 
-    enum BooleanPreference {
+    public enum BooleanPreference {
         INVARIANT_CHECK(true), MODEL_CHECK(true), INITIAL_CHECK(true);
 
         private boolean defaultValue;
