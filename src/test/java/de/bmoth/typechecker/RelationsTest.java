ed2e80a9c4378edd585d652e95181374c7e45b58
Sebastian Krings <sebastian@krin.gs>
2017-05-30 15:33:09 +0200

    introduce fields for strings

--- src/test/java/de/bmoth/typechecker/RelationsTest.java
+++ src/test/java/de/bmoth/typechecker/RelationsTest.java
@@ -1,61 +1,64 @@
 package de.bmoth.typechecker;
 
-import static de.bmoth.typechecker.TestTypechecker.getFormulaTypes;
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 import java.util.Map;
 
-import org.junit.Test;
+import static de.bmoth.typechecker.TestTypechecker.getFormulaTypes;
+import static org.junit.Assert.assertEquals;
 
 public class RelationsTest {
+    private static final String INTEGER = "INTEGER";
+    private static final String POW_INTEGER = "POW(INTEGER)";
+    private static final String POW_INTEGER_X_INTEGER = "POW(INTEGER*INTEGER)";
 
     @Test
     public void testDirectProduct() {
         String formula = "a = {1 |-> TRUE} >< {b |-> 1}";
         Map<String, String> formulaTypes = getFormulaTypes(formula);
         assertEquals("POW(INTEGER*(BOOL*INTEGER))", formulaTypes.get("a"));
-        assertEquals("INTEGER", formulaTypes.get("b"));
+        assertEquals(INTEGER, formulaTypes.get("b"));
     }
 
     @Test
     public void testOverwriteRelation() {
         String formula = "k = {x|->2} <+ {1|->y}";
         Map<String, String> formulaTypes = getFormulaTypes(formula);
-        assertEquals("POW(INTEGER*INTEGER)", formulaTypes.get("k"));
-        assertEquals("INTEGER", formulaTypes.get("x"));
-        assertEquals("INTEGER", formulaTypes.get("y"));
+        assertEquals(POW_INTEGER_X_INTEGER, formulaTypes.get("k"));
+        assertEquals(INTEGER, formulaTypes.get("x"));
+        assertEquals(INTEGER, formulaTypes.get("y"));
     }
-    
+
     @Test
     public void testDomainRestriction() {
         String formula = "k = x <| {1|->1}";
         Map<String, String> formulaTypes = getFormulaTypes(formula);
         assertEquals("POW(INTEGER*INTEGER)", formulaTypes.get("k"));
-        assertEquals("POW(INTEGER)", formulaTypes.get("x"));
+        assertEquals(POW_INTEGER, formulaTypes.get("x"));
     }
 
     @Test
     public void testDomainSubstraction() {
         String formula = "k = x <<| {1|->1}";
         Map<String, String> formulaTypes = getFormulaTypes(formula);
-        assertEquals("POW(INTEGER*INTEGER)", formulaTypes.get("k"));
-        assertEquals("POW(INTEGER)", formulaTypes.get("x"));
+        assertEquals(POW_INTEGER_X_INTEGER, formulaTypes.get("k"));
+        assertEquals(POW_INTEGER, formulaTypes.get("x"));
     }
-    
+
     @Test
     public void testRangeRestriction() {
         String formula = "k = {1|->1} |> x";
         Map<String, String> formulaTypes = getFormulaTypes(formula);
         assertEquals("POW(INTEGER*INTEGER)", formulaTypes.get("k"));
-        assertEquals("POW(INTEGER)", formulaTypes.get("x"));
+        assertEquals(POW_INTEGER, formulaTypes.get("x"));
     }
 
     @Test
     public void testRangeSubstraction() {
         String formula = "k = {1|->1} |>> x";
         Map<String, String> formulaTypes = getFormulaTypes(formula);
-        assertEquals("POW(INTEGER*INTEGER)", formulaTypes.get("k"));
-        assertEquals("POW(INTEGER)", formulaTypes.get("x"));
+        assertEquals(POW_INTEGER_X_INTEGER, formulaTypes.get("k"));
+        assertEquals(POW_INTEGER, formulaTypes.get("x"));
     }
 
     @Test
@@ -64,5 +67,5 @@ public class RelationsTest {
         Map<String, String> formulaTypes = getFormulaTypes(formula);
         assertEquals("POW(BOOL*INTEGER)", formulaTypes.get("k"));
     }
-    
+
 }
