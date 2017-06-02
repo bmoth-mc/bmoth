2504383efa7b827eb63f33afc701b771f46f4e81
Jessica Petrasch <jessica.petrasch@uni-duesseldorf.de>
2017-06-02 20:07:48 +0200

    Show the comma where needed and refactorings

--- src/main/java/de/bmoth/app/PrettyPrinter.java
+++ src/main/java/de/bmoth/app/PrettyPrinter.java
@@ -1,6 +1,5 @@
 package de.bmoth.app;
 
-
 import com.microsoft.z3.Expr;
 import com.microsoft.z3.FuncDecl;
 import com.microsoft.z3.FuncInterp;
@@ -19,29 +18,32 @@ public class PrettyPrinter {
         FuncDecl[] constantDeclarations = model.getConstDecls();
         for (FuncDecl constantDeclaration : constantDeclarations) {
             try {
-                System.out.println("Declaration arity: " + constantDeclaration.getArity());
-                System.out.println("Declaration sort: " + constantDeclaration.getRange().getSortKind());
-                output.add(constantDeclaration.getName().toString() + "=");
+                StringJoiner declarationOutput;
                 if (constantDeclaration.getRange().getSortKind() != Z3_sort_kind.Z3_ARRAY_SORT) {
+                    // not a set (couple or constant)
                     Expr constantInterpretation = model.getConstInterp(constantDeclaration);
                     Expr[] constInterpretationArgs = constantInterpretation.getArgs();
                     if (constInterpretationArgs.length == 0) {
-                        output.add(model.getConstInterp(constantDeclaration).toString());
+                        // constant
+                        declarationOutput = new StringJoiner(", ", "", "");
+                        output.add(declarationOutput.add(constantDeclaration.getName().toString() + "=" +
+                            model.getConstInterp(constantDeclaration).toString()).toString());
                     } else {
+                        // couple
+                        declarationOutput = new StringJoiner(", ", "(", ")");
                         for (Expr constInterpretationArg : constInterpretationArgs) {
-                            System.out.println(constInterpretationArg);
-                            System.out.println(constInterpretationArg.getSort());
-                            output.add(processDeclaration(constInterpretationArg, model));
+                            declarationOutput.add(processDeclaration(constInterpretationArg, model));
                         }
+                        output.add(constantDeclaration.getName().toString() + "=" + declarationOutput.toString());
                     }
                 } else {
+                    // set
                     FuncInterp functionInterpretation = model.getFuncInterp(constantDeclaration);
                     System.out.println(functionInterpretation);
                     for (FuncInterp.Entry funcInterpEntry : functionInterpretation.getEntries()) {
                         System.out.println(funcInterpEntry);
                     }
                 }
-
             } catch (com.microsoft.z3.Z3Exception e) {
                 logger.log(Level.SEVERE, "Z3 exception while solving", e);
             }
@@ -49,74 +51,43 @@ public class PrettyPrinter {
     }
 
 
-    public String processDeclaration(Expr constantInterpretation, Model model){
-        System.out.println("Interpretation: " + constantInterpretation.toString());
-        for (Expr interpretationArgs : constantInterpretation.getArgs()) {
+    public String processDeclaration(Expr interpretation, Model model){
+        System.out.println("Interpretation: " + interpretation.toString());
+        for (Expr interpretationArgs : interpretation.getArgs()) {
             System.out.println(interpretationArgs);
         }
-        constantInterpretation.getSort();
-        if (constantInterpretation.getSort().toString().equals("couple")
-            || constantInterpretation.getSort().toString().equals("set")) {
-            // not a constant
-            if (constantInterpretation.getSort().toString().equals("couple")) {
-                // couple
-                System.out.println("Couple");
-                return formatCouple(constantInterpretation, model);
-            } else {
-                // set
-                return formatSet(constantInterpretation);
-            }
+        interpretation.getSort();
+        if (interpretation.getSort().toString().equals("couple")) {
+            System.out.println("Couple");
+            return formatCouple(interpretation, model);
+        } else if (interpretation.getSort().toString().equals("set")) {
+            System.out.println("Set");
+            return formatSet(interpretation);
         } else {
             // constant
-            System.out.println("Constant " + constantInterpretation.toString());
-            return constantInterpretation.toString();
+            System.out.println("Constant " + interpretation.toString());
+            return interpretation.toString();
         }
-
-
-        /*if (constantInterpretation.getRange().getSortKind() == Z3_sort_kind.Z3_ARRAY_SORT
-            || constantInterpretation.getRange().getSortKind() == Z3_sort_kind.Z3_DATATYPE_SORT) {
-            // not a constant
-            if (constantInterpretation.getRange().getSortKind() == Z3_sort_kind.Z3_DATATYPE_SORT) {
-                // couple
-                System.out.println("Couple");
-                if (constantInterpretation.getArity() == 0) {
-                    return formatCouple(model.getConstInterp(constantInterpretation), model);
-                } else {
-                    System.out.println("Arity != 0");
-                    FuncInterp interp =  model.getFuncInterp(constantInterpretation);
-                    System.out.println("Interp: " + interp.toString());
-                    return "1";
-                }
-            } else {
-                // set
-                return formatSet(model.getFuncInterp(constantInterpretation));
-            }
-        } else {
-            System.out.println("Constant " + model.getConstInterp(constantInterpretation).toString());
-            // constant
-            return model.getConstInterp(constantInterpretation).toString();
-        }*/
     }
 
 
-    public String formatCouple(Expr constantInterpretation, Model model) {
+    public String formatCouple(Expr interpretation, Model model) {
         StringJoiner coupleJoiner = new StringJoiner(",", "(", ")");
-        for (Expr element : constantInterpretation.getArgs()) {
+        for (Expr element : interpretation.getArgs()) {
             if (element.isNumeral()) {
-                // System.out.println(element.toString() + " is numeral!");
                 coupleJoiner.add(element.toString());
             } else {
-                // System.out.println(element.toString() + " is not numeral!");
                 coupleJoiner.add(processDeclaration(element, model));
             }
         }
         return coupleJoiner.toString();
     }
 
-    public String formatSet(Expr constantInterpretation) {
+
+    public String formatSet(Expr interpretation) {
         StringJoiner setJoiner = new StringJoiner(",", "{", "}");
-        for (Expr constInterpretationArg : constantInterpretation.getArgs()) {
-            setJoiner.add(constantInterpretation.toString());
+        for (Expr constInterpretationArg : interpretation.getArgs()) {
+            setJoiner.add(constInterpretationArg.toString());
         }
         return setJoiner.toString();
     }
