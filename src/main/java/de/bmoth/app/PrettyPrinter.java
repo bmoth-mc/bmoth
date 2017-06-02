0e93405f04b80eb6da3761be56d8b857e0331398
Jessica Petrasch <jessica.petrasch@uni-duesseldorf.de>
2017-06-02 20:47:09 +0200

    Ignore sets in tuples by checking for null

--- src/main/java/de/bmoth/app/PrettyPrinter.java
+++ src/main/java/de/bmoth/app/PrettyPrinter.java
@@ -19,16 +19,13 @@ public class PrettyPrinter {
         for (FuncDecl constantDeclaration : constantDeclarations) {
             try {
                 if (constantDeclaration.getRange().getSortKind() != Z3_sort_kind.Z3_ARRAY_SORT) {
-                    // not a set (couple or constant)
                     Expr constantInterpretation = model.getConstInterp(constantDeclaration);
-                    if (constantInterpretation.getArgs().length == 0) { // constant
+                    if (constantInterpretation.getArgs().length == 0) // constant
                         output.add(constantDeclaration.getName().toString() + "=" + model.getConstInterp(constantDeclaration).toString());
-                    } else { // couple
+                    else // couple
                         output.add(constantDeclaration.getName().toString() + "=" + formatCouple(constantInterpretation, model));
-                    }
                 } else { // set
                     FuncInterp functionInterpretation = model.getFuncInterp(constantDeclaration);
-                    System.out.println(functionInterpretation);
                     output.add(constantDeclaration.getName().toString() + "="
                         + formatSet(functionInterpretation, model));
                 }
@@ -39,35 +36,22 @@ public class PrettyPrinter {
     }
 
 
-    public String processDeclaration(Expr interpretation, Model model){
-        System.out.println("Interpretation: " + interpretation.toString());
-        for (Expr interpretationArgs : interpretation.getArgs()) {
-            System.out.println(interpretationArgs);
-        }
-        interpretation.getSort();
-        if (interpretation.getSort().toString().equals("couple")) {
-            System.out.println("Couple");
+    public String processInterpretation(Expr interpretation, Model model){
+        if (interpretation.getSort().getSortKind() == Z3_sort_kind.Z3_DATATYPE_SORT)
             return formatCouple(interpretation, model);
-        } else if (interpretation.getSort().toString().equals("set")) {
-            System.out.println("Set");
-            // return formatSet(interpretation, model);
-            return "1";
-        } else {
-            // constant
-            System.out.println("Constant " + interpretation.toString());
+        else if (interpretation.getSort().getSortKind() == Z3_sort_kind.Z3_ARRAY_SORT)
+            return formatSet(model.getFuncInterp(interpretation.getFuncDecl()), model);
+        else // constant
             return interpretation.toString();
-        }
     }
 
 
     public String formatCouple(Expr interpretation, Model model) {
         StringJoiner coupleJoiner = new StringJoiner(",", "(", ")");
         for (Expr element : interpretation.getArgs()) {
-            if (element.isNumeral()) {
+            if (element.isNumeral())
                 coupleJoiner.add(element.toString());
-            } else {
-                coupleJoiner.add(processDeclaration(element, model));
-            }
+            else coupleJoiner.add(processInterpretation(element, model));
         }
         return coupleJoiner.toString();
     }
@@ -75,12 +59,11 @@ public class PrettyPrinter {
 
     public String formatSet(FuncInterp interpretation, Model model) {
         StringJoiner setJoiner = new StringJoiner(",", "{", "}");
-        for (FuncInterp.Entry interpretationEntry : interpretation.getEntries()) {
-            System.out.println(interpretationEntry);
-            if (interpretationEntry.getArgs()[0].isNumeral()) {
-                setJoiner.add(interpretationEntry.getArgs()[0].toString());
-            } else {
-                setJoiner.add(processDeclaration(interpretationEntry.getArgs()[0], model));
+        if (interpretation != null) {
+            for (FuncInterp.Entry interpretationEntry : interpretation.getEntries()) {
+                if (interpretationEntry.getArgs()[0].isNumeral())
+                    setJoiner.add(interpretationEntry.getArgs()[0].toString());
+                else setJoiner.add(processInterpretation(interpretationEntry.getArgs()[0], model));
             }
         }
         return setJoiner.toString();
