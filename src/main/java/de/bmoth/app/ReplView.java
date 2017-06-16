18ff19a460ba886e961f461e1ddc9f49ea13ea83
Sebastian Krings <sebastian@krin.gs>
2017-06-16 15:20:23 +0200

    move non-ui code to repl view model

--- src/main/java/de/bmoth/app/ReplView.java
+++ src/main/java/de/bmoth/app/ReplView.java
@@ -1,10 +1,5 @@
 package de.bmoth.app;
 
-import com.microsoft.z3.*;
-import de.bmoth.backend.z3.FormulaToZ3Translator;
-import de.bmoth.parser.Parser;
-import de.bmoth.parser.ParserException;
-import de.bmoth.parser.ast.nodes.FormulaNode;
 import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.InjectViewModel;
 import javafx.fxml.FXML;
@@ -15,90 +10,22 @@ import javafx.scene.input.KeyCode;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import static de.bmoth.parser.ast.nodes.FormulaNode.FormulaType.PREDICATE_FORMULA;
-
 public class ReplView implements FxmlView<ReplViewModel>, Initializable {
-
     @InjectViewModel
     ReplViewModel replViewModel;
     @FXML
     TextArea replText;
 
-    private Context ctx;
-    private Solver s;
-
     @Override
     public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
 
-        replViewModel.code.bind(replText.textProperty());
-
-        ctx = new Context();
-        s = ctx.mkSolver();
+        replViewModel.getCode().bindBidirectional(replText.textProperty());
 
         replText.setOnKeyPressed(keyEvent -> {
             if (keyEvent.getCode() == KeyCode.ENTER) {
-                String[] predicate = replViewModel.code.get().split("\n");
-                String solution = processPredicate(predicate[predicate.length - 1]);
-                replText.appendText(solution);
+                replViewModel.processPredicate();
+                keyEvent.consume();
             }
         });
-    }
-
-
-    private String processPredicate(String predicate) {
-        String formula = predicate;
-        ctx = new Context();
-        s = ctx.mkSolver();
-        FormulaNode node;
-        try {
-            node = Parser.getFormulaAsSemanticAst(predicate);
-        } catch (ParserException e) {
-            // TODO handle parser errors
-            throw new RuntimeException(e);//TODO replace this
-        }
-        boolean concatFlag = false;
-        if (node.getFormulaType() != PREDICATE_FORMULA) {
-            formula = "x=" + formula;
-            FormulaNode concatNode;
-            try {
-                concatNode = Parser.getFormulaAsSemanticAst(formula);
-                if (concatNode.getFormulaType() != PREDICATE_FORMULA) {
-                    throw new IllegalArgumentException("Input can not be extended to a predicate via an additional variable.");
-                } else {
-                    concatFlag = true;
-                }
-            } catch (ParserException e) {
-                // TODO handle parser errors
-                throw new RuntimeException(e);//TODO replace this
-            }
-
-        }
-        BoolExpr constraint;
-        try {
-            constraint = FormulaToZ3Translator.translatePredicate(formula, ctx);
-            s.add(constraint);
-            Status check = s.check();
-
-            if (check == Status.SATISFIABLE) {
-                Model model = s.getModel();
-                String output = new PrettyPrinter(model).getOutput();
-                if (model.toString().equals("")) {
-                    return "\n" + check;
-                } else {
-                    if (concatFlag) {
-                        String concatOutput = output.substring(3, output.length() - 1);
-                        return "\n" + concatOutput;
-                    } else {
-                        return "\n" + output;
-                    }
-                }
-            } else {
-                return "\n" + Status.UNSATISFIABLE;
-            }
-        } catch (ParserException e) {
-         // TODO handle parser errors
-            throw new RuntimeException(e);//TODO replace this
-        }
-
     }
 }
--- src/main/java/de/bmoth/app/ReplViewModel.java
+++ src/main/java/de/bmoth/app/ReplViewModel.java
@@ -1,24 +1,70 @@
 package de.bmoth.app;
 
+import com.microsoft.z3.*;
+import de.bmoth.backend.z3.FormulaToZ3Translator;
+import de.bmoth.parser.Parser;
+import de.bmoth.parser.ParserException;
+import de.bmoth.parser.ast.nodes.FormulaNode;
 import de.saxsys.mvvmfx.ViewModel;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
-/**
- * Created by julian on 15.06.17.
- */
+import static de.bmoth.parser.ast.nodes.FormulaNode.FormulaType.PREDICATE_FORMULA;
+
 public class ReplViewModel implements ViewModel {
 
-    SimpleStringProperty code = new SimpleStringProperty();
+    private StringProperty code = new SimpleStringProperty();
 
-    public String getCode() {
-        return code.get();
-    }
+    private Context ctx = new Context();
+    private Solver s = ctx.mkSolver();
 
-    public void setCode(String code) {
-        this.code.set(code);
-    }
-
-    public SimpleStringProperty codeProperty() {
+    StringProperty getCode() {
         return code;
+    }
+
+    void processPredicate() {
+        String predicate = code.get().substring(code.get().lastIndexOf('\n') + 1);
+        FormulaNode node;
+        try {
+            node = Parser.getFormulaAsSemanticAst(predicate);
+
+            boolean concatFlag = false;
+            if (node.getFormulaType() != PREDICATE_FORMULA) {
+                predicate = "x=" + predicate;
+                FormulaNode concatNode;
+                concatNode = Parser.getFormulaAsSemanticAst(predicate);
+                if (concatNode.getFormulaType() != PREDICATE_FORMULA) {
+                    throw new IllegalArgumentException("Input can not be extended to a predicate via an additional variable.");
+                } else {
+                    concatFlag = true;
+                }
+            }
+            BoolExpr constraint;
+
+            constraint = FormulaToZ3Translator.translatePredicate(predicate, ctx);
+            s.add(constraint);
+            Status check = s.check();
+
+            if (check == Status.SATISFIABLE) {
+                Model model = s.getModel();
+                String output = new PrettyPrinter(model).getOutput();
+                if (model.toString().equals("")) {
+                    code.set(code.get() + "\n" + check + "\n");
+                } else {
+                    if (concatFlag) {
+                        String concatOutput = output.substring(3, output.length() - 1);
+                        code.set(code.get() + "\n" + concatOutput + "\n");
+                    } else {
+                        code.set(code.get() + "\n" + output + "\n");
+                    }
+                }
+            } else {
+                code.set(code.get() + "\nUNSATISFIABLE\n");
+            }
+        } catch (ParserException e) {
+            // TODO handle parser errors
+            throw new RuntimeException(e);//TODO replace this
+        }
+
     }
 }
