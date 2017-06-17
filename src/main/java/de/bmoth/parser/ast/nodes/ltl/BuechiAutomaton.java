a3dc5992505a744c575a04c952f21431bb6fb2a5
Jessica Petrasch <jessica.petrasch@uni-duesseldorf.de>
2017-06-17 16:22:39 +0200

    Check if processed node is already in the nodesSet

--- src/main/java/de/bmoth/parser/ast/nodes/ltl/BuechiAutomaton.java
+++ src/main/java/de/bmoth/parser/ast/nodes/ltl/BuechiAutomaton.java
@@ -5,9 +5,25 @@ public class BuechiAutomaton {
     public String new_name() {
         return "place holder";
     }
-    
-    public BuechiAutomatonNode[] expand(BuechiAutomatonNode node, BuechiAutomatonNode[] nodes_set) {
-        return null;
+
+    public BuechiAutomatonNode nodeIsInNodeSet(BuechiAutomatonNode node, BuechiAutomatonNode[] nodesSet) {
+        BuechiAutomatonNode foundNode = null;
+        for (BuechiAutomatonNode nodeInSet: nodesSet) {
+            if ((nodeInSet.processed == node.processed) && (nodeInSet.next == node.next)) {
+                foundNode = nodeInSet;
+                break;
+            }
+        }
+        return foundNode;
+    }
+
+    public BuechiAutomatonNode[] expand(BuechiAutomatonNode node, BuechiAutomatonNode[] nodesSet) {
+        if (node.nonprocessed.length == 0) {
+            if (nodeIsInNodeSet(node, nodesSet) != null) {
+                return nodesSet;
+            }
+        }
+        return nodesSet;
     }
 
     public BuechiAutomatonNode[] create_graph(LTLFormula formula) {
