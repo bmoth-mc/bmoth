434bd185fa65c0d79643910deca97fe386bddad5
heinzware <ch_1992@hotmail.de>
2017-06-20 11:50:31 +0200

    removed code smells

--- src/test/java/de/bmoth/typechecker/MachinesTest.java
+++ src/test/java/de/bmoth/typechecker/MachinesTest.java
@@ -14,67 +14,76 @@ public class MachinesTest {
     private static final String POW_INTEGER = "POW(INTEGER)";
     private static final String INTEGER = "INTEGER";
 
+    private static final String MACHINE_NAME = "MACHINE test\n";
+    private static final String ONE_CONSTANT = "CONSTANTS k \n";
+    private static final String TWO_CONSTANTS = "CONSTANTS k,k2 \n";
+    private static final String THREE_CONSTANTS = "CONSTANTS k,k2,k3 \n";
+    private static final String VARIABLES_X_Y = "VARIABLES x,y \n";
+    private static final String INVARIANT_X_Y = "INVARIANT x=1 & y : BOOL \n";
+    private static final String INITIALISATION_X_1_Y_TRUE = "INITIALISATION x,y:= 1,TRUE \n";
+
+
     @Test
     public void testInteger() {
-        String machine = "MACHINE test\n" + "CONSTANTS k \n" + "PROPERTIES k = INTEGER \n" + "END";
+        String machine = MACHINE_NAME + ONE_CONSTANT + "PROPERTIES k = INTEGER \n" + "END";
         TestTypechecker t = new TestTypechecker(machine);
         assertEquals(POW_INTEGER, t.getConstants().get("k"));
     }
 
     @Test
     public void testIntegerException() {
-        String machine = "MACHINE test\n" + "CONSTANTS k \n" + "PROPERTIES 1 = INTEGER \n" + "END";
+        String machine = MACHINE_NAME + ONE_CONSTANT + "PROPERTIES 1 = INTEGER \n" + "END";
         typeCheckMachineAndGetErrorMessage(machine);
     }
 
     @Test
     public void testNatural() {
-        String machine = "MACHINE test\n" + "CONSTANTS k \n" + "PROPERTIES k = NATURAL \n" + "END";
+        String machine = MACHINE_NAME + ONE_CONSTANT + "PROPERTIES k = NATURAL \n" + "END";
         TestTypechecker t = new TestTypechecker(machine);
         assertEquals(POW_INTEGER, t.getConstants().get("k"));
     }
 
     @Test
     public void testNaturalException() {
-        String machine = "MACHINE test\n" + "CONSTANTS k \n" + "PROPERTIES 1 = NATURAL \n" + "END";
+        String machine = MACHINE_NAME + ONE_CONSTANT + "PROPERTIES 1 = NATURAL \n" + "END";
         typeCheckMachineAndGetErrorMessage(machine);
     }
 
     @Test
     public void testNatural1() {
-        String machine = "MACHINE test\n" + "CONSTANTS k \n" + "PROPERTIES k = NATURAL1 \n" + "END";
+        String machine = MACHINE_NAME + ONE_CONSTANT + "PROPERTIES k = NATURAL1 \n" + "END";
         TestTypechecker t = new TestTypechecker(machine);
         assertEquals(POW_INTEGER, t.getConstants().get("k"));
     }
 
     @Test
     public void testNatural1Exception() {
-        String machine = "MACHINE test\n" + "CONSTANTS k \n" + "PROPERTIES 1 = NATURAL1 \n" + "END";
+        String machine = MACHINE_NAME + ONE_CONSTANT + "PROPERTIES 1 = NATURAL1 \n" + "END";
         typeCheckMachineAndGetErrorMessage(machine);
     }
 
     @Test
     public void testInterval() {
-        String machine = "MACHINE test\n" + "CONSTANTS k \n" + "PROPERTIES k = 1..3 \n" + "END";
+        String machine = MACHINE_NAME + ONE_CONSTANT + "PROPERTIES k = 1..3 \n" + "END";
         TestTypechecker t = new TestTypechecker(machine);
         assertEquals(POW_INTEGER, t.getConstants().get("k"));
     }
 
     @Test
     public void testIntervalException() {
-        String machine = "MACHINE test\n" + "CONSTANTS k \n" + "PROPERTIES 1 = 1..3 \n" + "END";
+        String machine = MACHINE_NAME + ONE_CONSTANT + "PROPERTIES 1 = 1..3 \n" + "END";
         typeCheckMachineAndGetErrorMessage(machine);
     }
 
     @Test
     public void testIntervalException2() {
-        String machine = "MACHINE test\n" + "CONSTANTS k \n" + "PROPERTIES k = TRUE..3 \n" + "END";
+        String machine = MACHINE_NAME + ONE_CONSTANT + "PROPERTIES k = TRUE..3 \n" + "END";
         typeCheckMachineAndGetErrorMessage(machine);
     }
 
     @Test
     public void testGreaterThan() {
-        String machine = "MACHINE test\n" + "CONSTANTS k,k2 \n" + "PROPERTIES k > k2 \n" + "END";
+        String machine = MACHINE_NAME + TWO_CONSTANTS + "PROPERTIES k > k2 \n" + "END";
         TestTypechecker t = new TestTypechecker(machine);
         assertEquals(INTEGER, t.getConstants().get("k"));
         assertEquals(INTEGER, t.getConstants().get("k2"));
@@ -82,13 +91,13 @@ public class MachinesTest {
 
     @Test
     public void testGreaterThanException() {
-        String machine = "MACHINE test\n" + "PROPERTIES TRUE > 1 \n" + "END";
+        String machine = MACHINE_NAME + "PROPERTIES TRUE > 1 \n" + "END";
         typeCheckMachineAndGetErrorMessage(machine);
     }
 
     @Test
     public void testLessThan() {
-        String machine = "MACHINE test\n" + "CONSTANTS k,k2 \n" + "PROPERTIES k < k2 \n" + "END";
+        String machine = MACHINE_NAME + TWO_CONSTANTS + "PROPERTIES k < k2 \n" + "END";
         TestTypechecker t = new TestTypechecker(machine);
         assertEquals(INTEGER, t.getConstants().get("k"));
         assertEquals(INTEGER, t.getConstants().get("k2"));
@@ -96,13 +105,13 @@ public class MachinesTest {
 
     @Test
     public void testLessThanException() {
-        String machine = "MACHINE test\n" + "PROPERTIES TRUE < 1 \n" + "END";
+        String machine = MACHINE_NAME + "PROPERTIES TRUE < 1 \n" + "END";
         typeCheckMachineAndGetErrorMessage(machine);
     }
 
     @Test
     public void testGreaterEquals() {
-        String machine = "MACHINE test\n" + "CONSTANTS k,k2 \n" + "PROPERTIES k >= k2 \n" + "END";
+        String machine = MACHINE_NAME + TWO_CONSTANTS + "PROPERTIES k >= k2 \n" + "END";
         TestTypechecker t = new TestTypechecker(machine);
         assertEquals(INTEGER, t.getConstants().get("k"));
         assertEquals(INTEGER, t.getConstants().get("k2"));
@@ -110,13 +119,13 @@ public class MachinesTest {
 
     @Test
     public void testGreaterEqualsException() {
-        String machine = "MACHINE test\n" + "PROPERTIES TRUE >= 1 \n" + "END";
+        String machine = MACHINE_NAME + "PROPERTIES TRUE >= 1 \n" + "END";
         typeCheckMachineAndGetErrorMessage(machine);
     }
 
     @Test
     public void testLessEquals() {
-        String machine = "MACHINE test\n" + "CONSTANTS k,k2 \n" + "PROPERTIES k <= k2 \n" + "END";
+        String machine = MACHINE_NAME + TWO_CONSTANTS + "PROPERTIES k <= k2 \n" + "END";
         TestTypechecker t = new TestTypechecker(machine);
         assertEquals(INTEGER, t.getConstants().get("k"));
         assertEquals(INTEGER, t.getConstants().get("k2"));
@@ -124,13 +133,13 @@ public class MachinesTest {
 
     @Test
     public void testLessEqualsException() {
-        String machine = "MACHINE test\n" + "PROPERTIES TRUE <= 1 \n" + "END";
+        String machine = MACHINE_NAME + "PROPERTIES TRUE <= 1 \n" + "END";
         typeCheckMachineAndGetErrorMessage(machine);
     }
 
     @Test
     public void testAdd() {
-        String machine = "MACHINE test\n" + "CONSTANTS k,k2,k3 \n" + "PROPERTIES k = k2 + k3 \n" + "END";
+        String machine = MACHINE_NAME + THREE_CONSTANTS + "PROPERTIES k = k2 + k3 \n" + "END";
         TestTypechecker t = new TestTypechecker(machine);
         assertEquals(INTEGER, t.getConstants().get("k"));
         assertEquals(INTEGER, t.getConstants().get("k2"));
@@ -139,13 +148,13 @@ public class MachinesTest {
 
     @Test
     public void testAddException() {
-        String machine = "MACHINE test\n" + "CONSTANTS k\n" + "PROPERTIES TRUE = 1 + 1 \n" + "END";
+        String machine = MACHINE_NAME + ONE_CONSTANT + "PROPERTIES TRUE = 1 + 1 \n" + "END";
         typeCheckMachineAndGetErrorMessage(machine);
     }
 
     @Test
     public void testSub() {
-        String machine = "MACHINE test\n" + "CONSTANTS k,k2,k3 \n" + "PROPERTIES k = k2 + k3 \n" + "END";
+        String machine = MACHINE_NAME + THREE_CONSTANTS + "PROPERTIES k = k2 + k3 \n" + "END";
         TestTypechecker t = new TestTypechecker(machine);
         assertEquals(INTEGER, t.getConstants().get("k"));
         assertEquals(INTEGER, t.getConstants().get("k2"));
@@ -154,13 +163,13 @@ public class MachinesTest {
 
     @Test
     public void testSubException() {
-        String machine = "MACHINE test\n" + "CONSTANTS k\n" + "PROPERTIES TRUE = 1 - 1 \n" + "END";
+        String machine = MACHINE_NAME + ONE_CONSTANT + "PROPERTIES TRUE = 1 - 1 \n" + "END";
         typeCheckMachineAndGetErrorMessage(machine);
     }
 
     @Test
     public void testDivision() {
-        String machine = "MACHINE test\n" + "CONSTANTS k,k2,k3 \n" + "PROPERTIES k = k2 / k3 \n" + "END";
+        String machine = MACHINE_NAME + THREE_CONSTANTS + "PROPERTIES k = k2 / k3 \n" + "END";
         TestTypechecker t = new TestTypechecker(machine);
         assertEquals(INTEGER, t.getConstants().get("k"));
         assertEquals(INTEGER, t.getConstants().get("k2"));
@@ -169,13 +178,13 @@ public class MachinesTest {
 
     @Test
     public void testDivisionException() {
-        String machine = "MACHINE test\n" + "CONSTANTS k\n" + "PROPERTIES TRUE = 1 / 1 \n" + "END";
+        String machine = MACHINE_NAME + ONE_CONSTANT + "PROPERTIES TRUE = 1 / 1 \n" + "END";
         typeCheckMachineAndGetErrorMessage(machine);
     }
 
     @Test
     public void testPowerOf() {
-        String machine = "MACHINE test\n" + "CONSTANTS k,k2,k3 \n" + "PROPERTIES k = k2 ** k3 \n" + "END";
+        String machine = MACHINE_NAME + THREE_CONSTANTS + "PROPERTIES k = k2 ** k3 \n" + "END";
         TestTypechecker t = new TestTypechecker(machine);
         assertEquals(INTEGER, t.getConstants().get("k"));
         assertEquals(INTEGER, t.getConstants().get("k2"));
@@ -184,13 +193,13 @@ public class MachinesTest {
 
     @Test
     public void testPowerOfException() {
-        String machine = "MACHINE test\n" + "CONSTANTS k\n" + "PROPERTIES TRUE = 1 ** 1 \n" + "END";
+        String machine = MACHINE_NAME + ONE_CONSTANT + "PROPERTIES TRUE = 1 ** 1 \n" + "END";
         typeCheckMachineAndGetErrorMessage(machine);
     }
 
     @Test
     public void testModulo() {
-        String machine = "MACHINE test\n" + "CONSTANTS k,k2,k3 \n" + "PROPERTIES k = k2 mod k3 \n" + "END";
+        String machine = MACHINE_NAME + THREE_CONSTANTS + "PROPERTIES k = k2 mod k3 \n" + "END";
         TestTypechecker t = new TestTypechecker(machine);
         assertEquals(INTEGER, t.getConstants().get("k"));
         assertEquals(INTEGER, t.getConstants().get("k2"));
@@ -199,16 +208,16 @@ public class MachinesTest {
 
     @Test
     public void testModuloException() {
-        String machine = "MACHINE test\n" + "CONSTANTS k\n" + "PROPERTIES TRUE = 1 mod 1 \n" + "END";
+        String machine = MACHINE_NAME + ONE_CONSTANT + "PROPERTIES TRUE = 1 mod 1 \n" + "END";
         typeCheckMachineAndGetErrorMessage(machine);
     }
 
     @Test
     public void testSubstitution() {
-        String machine = "MACHINE test \n";
-        machine += "VARIABLES x,y \n";
-        machine += "INVARIANT x=1 & y : BOOL \n";
-        machine += "INITIALISATION x,y:= 1,TRUE \n";
+        String machine = MACHINE_NAME;
+        machine += VARIABLES_X_Y;
+        machine += INVARIANT_X_Y;
+        machine += INITIALISATION_X_1_Y_TRUE;
         machine += "OPERATIONS foo = SELECT x < 2 THEN x := 2 END \n";
         machine += "END";
         TestTypechecker t = new TestTypechecker(machine);
@@ -218,8 +227,8 @@ public class MachinesTest {
 
     @Test
     public void testSetComprehension() {
-        String machine = "MACHINE test\n";
-        machine += "CONSTANTS k\n";
+        String machine = MACHINE_NAME;
+        machine += ONE_CONSTANT;
         machine += "PROPERTIES k = {x | x : INTEGER } \n";
         machine += "END";
         TestTypechecker t = new TestTypechecker(machine);
@@ -228,10 +237,10 @@ public class MachinesTest {
 
     @Test
     public void testAnySubstitution() {
-        String machine = "MACHINE test \n";
-        machine += "VARIABLES x,y \n";
-        machine += "INVARIANT x=1 & y : BOOL \n";
-        machine += "INITIALISATION x,y:= 1,TRUE \n";
+        String machine = MACHINE_NAME;
+        machine += VARIABLES_X_Y;
+        machine += INVARIANT_X_Y;
+        machine += INITIALISATION_X_1_Y_TRUE;
         machine += "OPERATIONS foo = ANY p WHERE 1=1 THEN x := p END \n";
         machine += "END";
         MachineNode machineAsSemanticAst = parseMachine(machine);
@@ -243,10 +252,10 @@ public class MachinesTest {
 
     @Test
     public void testPreSubstitution() {
-        String machine = "MACHINE test \n";
-        machine += "VARIABLES x,y \n";
-        machine += "INVARIANT x=1 & y : BOOL \n";
-        machine += "INITIALISATION x,y:= 1,TRUE \n";
+        String machine = MACHINE_NAME;
+        machine += VARIABLES_X_Y;
+        machine += INVARIANT_X_Y;
+        machine += INITIALISATION_X_1_Y_TRUE;
         machine += "OPERATIONS foo = PRE x < 2 THEN x := 2 END \n";
         machine += "END";
         TestTypechecker t = new TestTypechecker(machine);
@@ -256,9 +265,9 @@ public class MachinesTest {
 
     @Test
     public void testBecomesSuchThatSubstitution() {
-        String machine = "MACHINE test \n";
-        machine += "VARIABLES x,y \n";
-        machine += "INVARIANT x=1 & y : BOOL \n";
+        String machine = MACHINE_NAME;
+        machine += VARIABLES_X_Y;
+        machine += INVARIANT_X_Y;
         machine += "INITIALISATION x,y:(x = 1 & y = TRUE) \n";
         machine += "END";
         TestTypechecker t = new TestTypechecker(machine);
@@ -268,7 +277,7 @@ public class MachinesTest {
 
     @Test
     public void testBecomesElementOfSubstitution() {
-        String machine = "MACHINE test \n";
+        String machine = MACHINE_NAME;
         machine += "VARIABLES a,b,c,d \n";
         machine += "INVARIANT a : {b,c,1} & d : BOOL \n";
         machine += "INITIALISATION a::{1} || b,c,d::INTEGER*{2}*BOOL \n";
@@ -282,7 +291,7 @@ public class MachinesTest {
 
     @Test
     public void testIfSubstitution() {
-        String machine = "MACHINE test \n";
+        String machine = MACHINE_NAME;
         machine += "VARIABLES a \n";
         machine += "INVARIANT a : INTEGER \n";
         machine += "INITIALISATION IF 1=1 THEN a := 1 ELSE a:= 2 END \n";
@@ -293,7 +302,7 @@ public class MachinesTest {
 
     @Test
     public void testSkipSubstitution() {
-        String machine = "MACHINE test \n";
+        String machine = MACHINE_NAME;
         machine += "INITIALISATION skip \n";
         machine += "END";
         new TestTypechecker(machine);
