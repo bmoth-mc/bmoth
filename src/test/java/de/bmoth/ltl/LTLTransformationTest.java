be9e9cdbc4b19290bd639010533d9795ed032ede
Sebastian Krings <sebastian@krin.gs>
2017-06-21 10:42:21 +0200

    turn test stub into real test

--- src/test/java/de/bmoth/ltl/LTLTransformationTest.java
+++ src/test/java/de/bmoth/ltl/LTLTransformationTest.java
@@ -1,12 +1,13 @@
 package de.bmoth.ltl;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
 import de.bmoth.backend.ltl.LTLTransformations;
 import de.bmoth.parser.Parser;
 import de.bmoth.parser.ParserException;
 import de.bmoth.parser.ast.nodes.ltl.LTLFormula;
 import de.bmoth.parser.ast.nodes.ltl.LTLNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class LTLTransformationTest {
 
@@ -14,9 +15,7 @@ public class LTLTransformationTest {
     public void testTransformationNotGloballyToFinallyNot() throws ParserException {
         String formula = "not(G { 1=1 })";
         LTLFormula ltlFormula = Parser.getLTLFormulaAsSemanticAst(formula);
-        System.out.println(ltlFormula.getLTLNode());
         LTLNode node1 = LTLTransformations.transformLTLNode(ltlFormula.getLTLNode());
-        System.out.println(node1);
-
+        assertEquals("FINALLY(NOT(EQUAL(1,1)))", node1.toString());
     }
 }
