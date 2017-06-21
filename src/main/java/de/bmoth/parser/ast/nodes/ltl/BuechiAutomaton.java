641b7aa0ca3b4b582c2119a0a21823286f9617f1
Jessica Petrasch <jessica.petrasch@uni-duesseldorf.de>
2017-06-21 09:25:47 +0200

    Node Case 3: Until, weak-until, or; Add helper functions new1, new2 and next1

--- src/main/java/de/bmoth/parser/ast/nodes/ltl/BuechiAutomaton.java
+++ src/main/java/de/bmoth/parser/ast/nodes/ltl/BuechiAutomaton.java
@@ -26,6 +26,34 @@ public class BuechiAutomaton {
         return foundNode;
     }
 
+    public List<LTLNode> new1(LTLInfixOperatorNode node) {
+        List<LTLNode> newNodes = new ArrayList<>();
+        if ((node.getKind() == LTLInfixOperatorNode.Kind.UNTIL) || (node.getKind() == LTLInfixOperatorNode.Kind.OR)) {
+            newNodes.add(node.getLeft());
+        }
+        // TODO: weak-until
+        return newNodes;
+    }
+
+    public List<LTLNode> new2(LTLInfixOperatorNode node) {
+        List<LTLNode> newNodes = new ArrayList<>();
+        if (node.getKind() == LTLInfixOperatorNode.Kind.UNTIL) {
+            newNodes.add(node);
+        }
+        // In case of OR an empty list is returned
+        // TODO: weak-until
+        return newNodes;
+    }
+
+    public List<LTLNode> next1(LTLInfixOperatorNode node) {
+        List<LTLNode> newNodes = new ArrayList<>();
+        if ((node.getKind() == LTLInfixOperatorNode.Kind.UNTIL) || (node.getKind() == LTLInfixOperatorNode.Kind.OR)) {
+            newNodes.add(node.getRight());
+        }
+        // TODO: weak-until
+        return newNodes;
+    }
+
     public List<BuechiAutomatonNode> expand(BuechiAutomatonNode node, List<BuechiAutomatonNode> nodesSet) {
         if (node.unprocessed.size() == 0) {
             // The current node is completely processed and can be added to the list (or, in case he was
@@ -63,7 +91,36 @@ public class BuechiAutomaton {
                 next.add(((LTLPrefixOperatorNode) formula).getArgument());
                 return expand(new BuechiAutomatonNode(node.name, node.incoming, node.unprocessed,
                     processed, next), nodesSet);
-            } 
+            } else
+
+                // Until, weak-until, logical or: Split the node in two
+                if (formula instanceof LTLInfixOperatorNode) {
+                    // Prepare the parts for the first new node
+                    List<LTLNode> unprocessed = new ArrayList<>(node.unprocessed);
+                    unprocessed.addAll(new1((LTLInfixOperatorNode) formula));
+                    unprocessed.removeAll(node.processed);
+
+                    List<LTLNode> processed = new ArrayList<>(node.processed);
+                    processed.add(formula);
+
+                    List<LTLNode> next = new ArrayList<>(node.next);
+                    next.addAll(next1((LTLInfixOperatorNode) formula));
+
+                    // Create the first new node
+                    BuechiAutomatonNode node1 = new BuechiAutomatonNode(new_name(), node.incoming,
+                        unprocessed, processed, next);
+
+                    // Prepare the parts for the second new node
+                    unprocessed = new ArrayList<>(node.unprocessed);
+                    unprocessed.addAll(new2((LTLInfixOperatorNode) formula));
+                    unprocessed.removeAll(node.processed);
+
+                    // Create the second new node
+                    BuechiAutomatonNode node2 = new BuechiAutomatonNode(new_name(), node.incoming,
+                        unprocessed, processed, node.next);
+
+                    return expand(node2, expand(node1, nodesSet));
+            }
         }
         return nodesSet;
     }
