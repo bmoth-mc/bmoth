30cc4e0bcfb5853aed18665c275b94b580851ea6
xmoex <moritz.gericke@uni-duesseldorf.de>
2017-06-22 15:19:59 +0200

    modify TranslationOptions to also incorporate a non-primed state

--- src/main/java/de/bmoth/backend/TranslationOptions.java
+++ src/main/java/de/bmoth/backend/TranslationOptions.java
@@ -2,16 +2,22 @@ package de.bmoth.backend;
 
 public class TranslationOptions {
     private int primeLevel;
+    private final boolean hasPrimeLevel;
 
     public TranslationOptions() {
-        this(0);
+        this.hasPrimeLevel = false;
     }
 
     public TranslationOptions(int primeLevel) {
+        this.hasPrimeLevel = true;
         this.primeLevel = primeLevel;
     }
 
     public int getPrimeLevel() {
         return primeLevel;
+    }
+
+    public boolean isHasPrimeLevel() {
+        return hasPrimeLevel;
     }
 }
