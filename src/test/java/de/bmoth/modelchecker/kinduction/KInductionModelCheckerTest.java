b6373c7cd1854551221e2d8addb6dc52b1b2314d
Sebastian Krings <sebastian@krin.gs>
2017-06-22 12:42:47 +0200

    add test cases for kinduction

---
+++ src/test/java/de/bmoth/modelchecker/kinduction/KInductionModelCheckerTest.java
@@ -0,0 +1,44 @@
+package de.bmoth.modelchecker.kinduction;
+
+import de.bmoth.TestParser;
+import de.bmoth.modelchecker.kind.KinductionModelChecker;
+import de.bmoth.modelchecker.kind.KinductionModelCheckingResult;
+import org.junit.Test;
+
+import static de.bmoth.modelchecker.kind.KinductionModelCheckingResult.Type.COUNTER_EXAMPLE_FOUND;
+import static de.bmoth.modelchecker.kind.KinductionModelCheckingResult.Type.EXCEEDED_MAX_STEPS;
+import static org.junit.Assert.assertEquals;
+
+public class KInductionModelCheckerTest extends TestParser {
+
+    @Test
+    public void test1() {
+        String machine = "MACHINE infInc\n" +
+            "VARIABLES c\n" +
+            "INVARIANT c : NATURAL\n" +
+            "INITIALISATION c := 0\n" +
+            "OPERATIONS\n" +
+            "\tinc = BEGIN c := c + 1 END\n" +
+            "END\n";
+
+        KinductionModelCheckingResult result = new KinductionModelChecker(parseMachine(machine), 20).check();
+        assertEquals(EXCEEDED_MAX_STEPS, result.getType());
+    }
+
+    @Test
+    public void test2() {
+        String machine = "MACHINE ebr\n" +
+            "VARIABLES c, b\n" +
+            "INVARIANT c : INTEGER &\n" +
+            "\tb = TRUE\n" +
+            "INITIALISATION c := 0 || b := TRUE\n" +
+            "OPERATIONS\n" +
+            "\tinc = ANY x WHERE x:INTEGER THEN c := c + x END;\n" +
+            "\terr = PRE c > 99999 THEN b := FALSE END\n" +
+            "END\n";
+
+        KinductionModelCheckingResult result = new KinductionModelChecker(parseMachine(machine), 20).check();
+        assertEquals(COUNTER_EXAMPLE_FOUND, result.getType());
+        assertEquals("{b=false, c=100000}", result.getLastState().toString());
+    }
+}
