758a81883f7ee77f3e357584dc454201ae6ce316
Lunnaris01 <Julian.Ullrich@Uni-Duesseldorf.de>
2017-06-23 13:43:33 +0200

    implement KINDUCTION

--- src/main/java/de/bmoth/modelchecker/kind/KInductionModelCheckingResult.java
+++ src/main/java/de/bmoth/modelchecker/kind/KInductionModelCheckingResult.java
@@ -3,21 +3,15 @@ package de.bmoth.modelchecker.kind;
 import de.bmoth.modelchecker.State;
 
 public class KInductionModelCheckingResult {
-    public enum Type {
-        COUNTER_EXAMPLE_FOUND,
-        EXCEEDED_MAX_STEPS
-    }
 
     private final State lastState;
     private final int steps;
     private final Type type;
-
     private KInductionModelCheckingResult(State lastState, int steps, Type type) {
         this.lastState = lastState;
         this.steps = steps;
         this.type = type;
     }
-
 
     static KInductionModelCheckingResult createCounterExampleFound(State lastState, int steps) {
         return new KInductionModelCheckingResult(lastState, steps, Type.COUNTER_EXAMPLE_FOUND);
@@ -25,6 +19,10 @@ public class KInductionModelCheckingResult {
 
     static KInductionModelCheckingResult createExceededMaxSteps(int maxSteps) {
         return new KInductionModelCheckingResult(null, maxSteps, Type.EXCEEDED_MAX_STEPS);
+    }
+
+    public static KInductionModelCheckingResult createVerfifiedViaInduction(int step) {
+        return new KInductionModelCheckingResult(null, step, Type.VERFIED);
     }
 
     public State getLastState() {
@@ -37,5 +35,11 @@ public class KInductionModelCheckingResult {
 
     public int getSteps() {
         return steps;
+    }
+
+    public enum Type {
+        COUNTER_EXAMPLE_FOUND,
+        EXCEEDED_MAX_STEPS,
+        VERFIED
     }
 }
