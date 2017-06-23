491fa5ed9177c228462e4c0fb6a5c989300be7c4
Mareikes <Mareike.Mutz@uni-duesseldorf.de>
2017-06-23 12:42:49 +0200

    merge

--- src/main/java/de/bmoth/backend/ltl/LTLTransformations.java
+++ src/main/java/de/bmoth/backend/ltl/LTLTransformations.java
@@ -1,6 +1,6 @@
 package de.bmoth.backend.ltl;
 
-<<<<<<< HEAD
+
 
 import de.bmoth.backend.ltl.transformation.ConvertFinallyGloballyFinallyToGloballyFinally;
 import de.bmoth.backend.ltl.transformation.ConvertGloballyFinallyGloballyToFinallyGlobally;
@@ -11,9 +11,7 @@ import de.bmoth.backend.ltl.transformation.ConvertNotGloballyToFinallyNot;
 import de.bmoth.backend.ltl.transformation.ConvertNotNextToNextNot;
 import de.bmoth.backend.ltl.transformation.ConvertPhiUntilPhiUntilPsiToPhiUntilPsi;
 import de.bmoth.parser.ast.nodes.ltl.LTLFormula;
-=======
 import de.bmoth.backend.ltl.transformation.*;
->>>>>>> 0500f2415138555a9f7dcecad560111b3bd4f6cd
 import de.bmoth.parser.ast.nodes.ltl.LTLNode;
 import de.bmoth.parser.ast.visitors.ASTTransformationVisitor;
 import de.bmoth.parser.ast.visitors.AbstractASTTransformation;
