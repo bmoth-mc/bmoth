bb9252743b1d53a93e483d53095b0f239919a17b
Sebastian Krings <sebastian@krin.gs>
2017-06-23 12:40:17 +0200

    add test verifying if LTL not is pushed into the B predicate

--- src/test/java/de/bmoth/ltl/LTLTransformationTest.java
+++ src/test/java/de/bmoth/ltl/LTLTransformationTest.java
@@ -3,11 +3,15 @@ package de.bmoth.ltl;
 import de.bmoth.backend.ltl.LTLTransformations;
 import de.bmoth.parser.Parser;
 import de.bmoth.parser.ParserException;
+import de.bmoth.parser.ast.nodes.ltl.LTLBPredicateNode;
 import de.bmoth.parser.ast.nodes.ltl.LTLFormula;
 import de.bmoth.parser.ast.nodes.ltl.LTLNode;
+import de.bmoth.parser.ast.nodes.ltl.LTLPrefixOperatorNode;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class LTLTransformationTest {
 
@@ -49,5 +53,22 @@ public class LTLTransformationTest {
         LTLFormula ltlFormula = Parser.getLTLFormulaAsSemanticAst(formula);
         LTLNode node1 = LTLTransformations.transformLTLNode(ltlFormula.getLTLNode());
         assertEquals("UNTIL(EQUAL(1,1),EQUAL(2,2))", node1.toString());
+    }
+
+    @Test
+    @Ignore
+    public void testTransformation6() throws ParserException {
+        String formula = "not(G { 1=1 })";
+        LTLFormula ltlFormula = Parser.getLTLFormulaAsSemanticAst(formula);
+        LTLNode node1 = LTLTransformations.transformLTLNode(ltlFormula.getLTLNode());
+
+        assertEquals("FINALLY(NOT(EQUAL(1,1)))", node1.toString());
+
+        // check if we have the B not, not the LTL not
+        assertTrue(node1 instanceof LTLPrefixOperatorNode);
+        LTLPrefixOperatorNode node1PO = (LTLPrefixOperatorNode) node1;
+        assertEquals(LTLPrefixOperatorNode.Kind.FINALLY, node1PO.getKind());
+
+        assertTrue(node1PO.getArgument() instanceof LTLBPredicateNode);
     }
 }
