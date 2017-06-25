c0647edffa1d625746caaea386eafd6343dd253d
xmoex <moritz.gericke@uni-duesseldorf.de>
2017-06-25 15:28:55 +0200

    implemented machine builder into test parser for better readability and to prevent duplicate code and

--- src/test/java/de/bmoth/TestParser.java
+++ src/test/java/de/bmoth/TestParser.java
@@ -5,6 +5,10 @@ import de.bmoth.parser.ParserException;
 import de.bmoth.parser.ast.nodes.FormulaNode;
 import de.bmoth.parser.ast.nodes.MachineNode;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.junit.Assert.fail;
 
 public class TestParser {
@@ -36,6 +40,78 @@ public class TestParser {
         } catch (ParserException e) {
             fail(e.getMessage());
             return null;
+        }
+    }
+
+    public class MachineBuilder {
+        private String name = "";
+        private String sets = "";
+        private String variables = "";
+        private String invariant = "";
+        private String initialization = "";
+        private List<String> operations = new ArrayList<>();
+
+        public MachineBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public MachineBuilder setVariables(String variables) {
+            this.variables = variables;
+            return this;
+        }
+
+        public MachineBuilder setInvariant(String invariant) {
+            this.invariant = invariant;
+            return this;
+        }
+
+        public MachineBuilder setInitialization(String initialization) {
+            this.initialization = initialization;
+            return this;
+        }
+
+        public MachineBuilder addOperation(String operation) {
+            this.operations.add(operation);
+            return this;
+        }
+
+
+        public MachineNode build() {
+            return parseMachine(this.toString());
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append("MACHINE ").append(name).append("\n");
+            if(!sets.isEmpty()){
+                sb.append("SETS ").append(sets).append("\n");
+            }
+            if (!variables.isEmpty()) {
+                sb.append("VARIABLES ").append(variables).append("\n");
+            }
+            if (!invariant.isEmpty()) {
+                sb.append("INVARIANT ").append(invariant).append("\n");
+            }
+            if (!initialization.isEmpty()) {
+                sb.append("INITIALISATION ").append(initialization).append("\n");
+            }
+
+            if (!operations.isEmpty()) {
+                sb.append("OPERATIONS\n");
+                for (Iterator<String> op = operations.iterator(); op.hasNext(); ) {
+                    String operation = op.next();
+                    sb.append("\t").append(operation).append(op.hasNext() ? ";\n" : "\n");
+                }
+            }
+            sb.append("END");
+            return sb.toString();
+        }
+
+        public MachineBuilder setSets(String sets) {
+            this.sets = sets;
+            return this;
         }
     }
 
