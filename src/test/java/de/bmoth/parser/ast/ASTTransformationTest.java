96507f438039b020d577bb2b28147f6fc779737c
Sebastian Krings <sebastian@krin.gs>
2017-06-27 10:46:55 +0200

    remove rewrite rule

--- src/test/java/de/bmoth/parser/ast/ASTTransformationTest.java
+++ src/test/java/de/bmoth/parser/ast/ASTTransformationTest.java
@@ -1,28 +1,14 @@
 package de.bmoth.parser.ast;
 
-import static de.bmoth.parser.ast.nodes.FormulaNode.FormulaType.*;
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
-
 import de.bmoth.backend.z3.AstTransformationsForZ3;
-import de.bmoth.parser.ast.nodes.ExprNode;
-import de.bmoth.parser.ast.nodes.ExpressionOperatorNode;
 import de.bmoth.parser.ast.nodes.FormulaNode;
 import de.bmoth.parser.ast.nodes.PredicateNode;
-import static de.bmoth.TestParser.*;
+import org.junit.Test;
+
+import static de.bmoth.TestParser.parseFormula;
+import static org.junit.Assert.assertEquals;
+
 public class ASTTransformationTest {
-
-    @Test
-    public void testFlattenUnion() {
-        String formula = "({1} \\/ a) \\/ b";
-        FormulaNode formulaNode = parseFormula(formula);
-        assertEquals(EXPRESSION_FORMULA, formulaNode.getFormulaType());
-        ExprNode transformExprNode = AstTransformationsForZ3
-                .transformExprNode((ExpressionOperatorNode) formulaNode.getFormula());
-        assertEquals("UNION(SET_ENUMERATION(1),a,b)", transformExprNode.toString());
-    }
-
     @Test
     public void testElementOfCombinedWithUnion() {
         String formula = "a : {1} \\/ b";
