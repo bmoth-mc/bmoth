442f1b1f1329a28ff23f62c7c480ef4c3facb38c
Sebastian Krings <sebastian@krin.gs>
2017-06-27 11:02:29 +0200

    add transformation rule

--- src/test/java/de/bmoth/parser/ast/ASTTransformationTest.java
+++ src/test/java/de/bmoth/parser/ast/ASTTransformationTest.java
@@ -3,6 +3,7 @@ package de.bmoth.parser.ast;
 import de.bmoth.backend.z3.AstTransformationsForZ3;
 import de.bmoth.parser.ast.nodes.FormulaNode;
 import de.bmoth.parser.ast.nodes.PredicateNode;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static de.bmoth.TestParser.parseFormula;
@@ -15,5 +16,39 @@ public class ASTTransformationTest {
         FormulaNode formulaNode = parseFormula(formula);
         PredicateNode op = AstTransformationsForZ3.transformPredicate((PredicateNode) formulaNode.getFormula());
         assertEquals("OR(ELEMENT_OF(a,SET_ENUMERATION(1)),ELEMENT_OF(a,b))", op.toString());
+    }
+
+    @Test
+    @Ignore
+    public void testElementOfCombinedWithMultipleUnions() {
+        String formula = "a : {1} \\/ b \\/ c";
+        FormulaNode formulaNode = parseFormula(formula);
+        PredicateNode op = AstTransformationsForZ3.transformPredicate((PredicateNode) formulaNode.getFormula());
+        assertEquals("OR(ELEMENT_OF(a,SET_ENUMERATION(1)),ELEMENT_OF(a,b))", op.toString());
+    }
+
+    @Test
+    public void testElementOfCombinedWithIntersection() {
+        String formula = "a : {1} /\\ b";
+        FormulaNode formulaNode = parseFormula(formula);
+        PredicateNode op = AstTransformationsForZ3.transformPredicate((PredicateNode) formulaNode.getFormula());
+        assertEquals("AND(ELEMENT_OF(a,SET_ENUMERATION(1)),ELEMENT_OF(a,b))", op.toString());
+    }
+
+    @Test
+    @Ignore
+    public void testElementOfCombinedWithMultipleIntersections() {
+        String formula = "a : {1} /\\ b /\\ c";
+        FormulaNode formulaNode = parseFormula(formula);
+        PredicateNode op = AstTransformationsForZ3.transformPredicate((PredicateNode) formulaNode.getFormula());
+        assertEquals("AND(ELEMENT_OF(a,SET_ENUMERATION(1)),ELEMENT_OF(a,b))", op.toString());
+    }
+
+    @Test
+    public void testMemberOfIntervalToLeqGeq() {
+        String formula = "a : 1..7";
+        FormulaNode formulaNode = parseFormula(formula);
+        PredicateNode op = AstTransformationsForZ3.transformPredicate((PredicateNode) formulaNode.getFormula());
+        assertEquals("AND(GREATER_EQUAL(a,1),LESS_EQUAL(a,7))", op.toString());
     }
 }
