34a650ab71f625edf3cdd1e5abdc5d332466067d
Mareikes <Mareike.Mutz@uni-duesseldorf.de>
2017-06-28 13:27:00 +0200

    conversion of weak_until to release for the buchi automaton

---
+++ src/main/java/de/bmoth/backend/ltl/transformation/ConvertWeakToRelease.java
@@ -0,0 +1,35 @@
+package de.bmoth.backend.ltl.transformation;
+
+
+
+import de.bmoth.parser.ast.nodes.Node;
+import de.bmoth.parser.ast.nodes.ltl.LTLInfixOperatorNode;
+import de.bmoth.parser.ast.nodes.ltl.LTLNode;
+import de.bmoth.parser.ast.visitors.LTLASTTransformation;
+
+public class ConvertWeakToRelease extends LTLASTTransformation{
+
+	@Override
+	public boolean canHandleNode(Node node) {
+		return isOperator(node, LTLInfixOperatorNode.Kind.WEAK_UNTIL); 
+	}
+	
+
+	@Override
+	public Node transformNode(Node oldNode) {
+		LTLInfixOperatorNode weakOperator = (LTLInfixOperatorNode) oldNode;
+		if(weakOperator.getKind()==LTLInfixOperatorNode.Kind.WEAK_UNTIL){
+			LTLNode weaksLeft = weakOperator.getLeft();
+            LTLNode weaksRight = weakOperator.getRight();
+            LTLNode leftOrRight = new LTLInfixOperatorNode(LTLInfixOperatorNode.Kind.OR, weaksLeft, weaksRight);
+            setChanged();
+            return new LTLInfixOperatorNode(LTLInfixOperatorNode.Kind.RELEASE, weaksRight, leftOrRight);
+            
+		}
+		
+		return oldNode;
+	}
+
+	
+
+}
