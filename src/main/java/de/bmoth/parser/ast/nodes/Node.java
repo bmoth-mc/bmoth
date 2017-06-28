52c453c7f30a225f841c77824cfe70f5659d23bb
xmoex <moritz.gericke@uni-duesseldorf.de>
2017-06-28 11:01:52 +0200

    added method to node handing a default implementation at this point to prevent compile errors

--- src/main/java/de/bmoth/parser/ast/nodes/Node.java
+++ src/main/java/de/bmoth/parser/ast/nodes/Node.java
@@ -1,5 +1,30 @@
 package de.bmoth.parser.ast.nodes;
 
+import java.util.List;
+
 public interface Node {
-    
+    default boolean sameClass(Node that) {
+        return this == that || that != null && this.getClass() == that.getClass();
+    }
+
+    default boolean equalAst(Node other) {
+        return false;
+    }
+
+    class ListAstEquals<T extends Node> {
+        boolean equalAst(List<T> first, List<T> second) {
+            if (first.size() != second.size()) {
+                return false;
+            }
+
+            // relies on same ordering of elements, e.g. x+1 != 1+x
+            for (int i = 0; i < first.size(); i++) {
+                if (first.get(i).equalAst(second.get(i))) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
 }
