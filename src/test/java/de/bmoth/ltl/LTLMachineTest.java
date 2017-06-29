03a97e75c1e98195fa893d32dd16f77b1576279d
xmoex <moritz.gericke@uni-duesseldorf.de>
2017-06-29 09:42:41 +0200

    removed unused import

--- src/test/java/de/bmoth/ltl/LTLMachineTest.java
+++ src/test/java/de/bmoth/ltl/LTLMachineTest.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
 
 public class LTLMachineTest extends TestParser {
 
