d076d892764d3edb7085e355336f050af6cf582b
xmoex <moritz.gericke@uni-duesseldorf.de>
2017-07-03 11:52:29 +0200

    fixed bug

--- src/main/java/de/bmoth/backend/ltl/transformation/RemoveTrueOr.java
+++ src/main/java/de/bmoth/backend/ltl/transformation/RemoveTrueOr.java
@@ -4,8 +4,7 @@ import de.bmoth.parser.ast.nodes.Node;
 import de.bmoth.parser.ast.nodes.ltl.LTLKeywordNode;
 import de.bmoth.parser.ast.visitors.ASTTransformation;
 
-import static de.bmoth.backend.ltl.LTLTransformationUtil.contains;
-import static de.bmoth.backend.ltl.LTLTransformationUtil.isOperator;
+import static de.bmoth.backend.ltl.LTLTransformationUtil.*;
 import static de.bmoth.parser.ast.nodes.ltl.LTLInfixOperatorNode.Kind.OR;
 import static de.bmoth.parser.ast.nodes.ltl.LTLKeywordNode.Kind.TRUE;
 
@@ -13,7 +12,9 @@ public class RemoveTrueOr implements ASTTransformation {
 
     @Override
     public boolean canHandleNode(Node node) {
-        return isOperator(node, OR) && contains(node, TRUE);
+        return isOperator(node, OR)
+            && (containsLeft(node, TRUE)
+            || containsRight(node, TRUE));
     }
 
     @Override
