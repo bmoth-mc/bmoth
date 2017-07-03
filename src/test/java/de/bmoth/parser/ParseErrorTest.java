e65b0ff136d120257419c012d0c7b15dd05da098
xmoex <moritz.gericke@uni-duesseldorf.de>
2017-07-03 14:11:53 +0200

    added init & teardown to prevent irritating logging output

--- src/test/java/de/bmoth/parser/ParseErrorTest.java
+++ src/test/java/de/bmoth/parser/ParseErrorTest.java
@@ -1,10 +1,31 @@
 package de.bmoth.parser;
 
+import org.junit.After;
+import org.junit.Before;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class ParseErrorTest {
+
+    private Level savedParserLevel;
+    private Logger parserLogger;
+
+    @Before
+    public void init() {
+        // prepare logger to be quiet during test (prevent irritating red stuff...)
+        parserLogger = Logger.getLogger(Parser.class.getName());
+        savedParserLevel = parserLogger.getLevel();
+        parserLogger.setLevel(Level.OFF);
+    }
+
+    @After
+    public void tearDown() {
+        // reset logger
+        parserLogger.setLevel(savedParserLevel);
+    }
 
     @Test
     public void testParseError() {
