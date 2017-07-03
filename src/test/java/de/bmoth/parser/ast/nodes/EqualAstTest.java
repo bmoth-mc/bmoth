6251c6756d15c85425a1b7b9fd8888d12fa162dc
Sebastian Krings <sebastian@krin.gs>
2017-07-03 13:23:08 +0200

    add test

--- src/test/java/de/bmoth/parser/ast/nodes/EqualAstTest.java
+++ src/test/java/de/bmoth/parser/ast/nodes/EqualAstTest.java
@@ -23,6 +23,29 @@ public class EqualAstTest extends TestParser {
             .addOperation("op_changed1 = IF a = 2 THEN a:=2 ELSE a:=1 END")
             .addOperation("op_changed2 = IF a = 3 THEN a:=5 ELSE a:=1 END")
             .addOperation("op_changed3 = IF a = 3 THEN a:=2 ELSE a:=3 END")
+            .addOperation("op_not_if = SELECT a = 3 THEN a:=2 END")
+            .build();
+
+        List<SubstitutionNode> substitutions = machine.getOperations().stream().map(OperationNode::getSubstitution).collect(Collectors.toList());
+        assertTrue(substitutions.get(0).equalAst(substitutions.get(1)));
+        assertFalse(substitutions.get(0).equalAst(substitutions.get(2)));
+        assertFalse(substitutions.get(0).equalAst(substitutions.get(3)));
+        assertFalse(substitutions.get(0).equalAst(substitutions.get(4)));
+        assertFalse(substitutions.get(0).equalAst(substitutions.get(5)));
+    }
+
+    @Test
+    public void testEqualAstSelectSubstitution() {
+        MachineNode machine = new MachineBuilder()
+            .setName("IfSubstitutionMachine")
+            .setVariables("a")
+            .setInvariant("a : INTEGER")
+            .setInitialization("a := 0")
+            .addOperation("op1 = SELECT a : {2+1} THEN a:=2 END")
+            .addOperation("op2 = SELECT a : {2+1} THEN a:=2 END")
+            .addOperation("op_changed1 = SELECT a = 3 THEN a:=2 END")
+            .addOperation("op_changed2 = SELECT a : {2+1} THEN a:=1+1 END")
+            .addOperation("op_not_select = IF a = 3 THEN a:=2 ELSE a:=3 END")
             .build();
 
         List<SubstitutionNode> substitutions = machine.getOperations().stream().map(OperationNode::getSubstitution).collect(Collectors.toList());
