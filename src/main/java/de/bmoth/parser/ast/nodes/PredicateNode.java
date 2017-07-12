5c3ef390ef25871053e5b532831996a882abe0e7
dohan <dohan001@hhu.de>
2017-07-12 13:54:23 +0200

    add method getNegatedPredicateNode

--- src/main/java/de/bmoth/parser/ast/nodes/PredicateNode.java
+++ src/main/java/de/bmoth/parser/ast/nodes/PredicateNode.java
@@ -1,11 +1,19 @@
 package de.bmoth.parser.ast.nodes;
 
+import java.util.Arrays;
+
 import org.antlr.v4.runtime.tree.ParseTree;
+
+import de.bmoth.parser.ast.nodes.PredicateOperatorNode.PredicateOperator;
 
 public abstract class PredicateNode extends TypedNode {
 
     public PredicateNode(ParseTree context) {
         super(context);
+    }
+
+    public PredicateNode getNegatedPredicateNode() {
+        return new PredicateOperatorNode(getParseTree(), PredicateOperator.NOT, Arrays.asList(this));
     }
 
 }
