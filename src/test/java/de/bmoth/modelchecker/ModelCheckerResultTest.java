d45dda4f120edfebef6fec039cc85448e39bf8ad
x-moe-x <moritz.gericke@uni-duesseldorf.de>
2017-07-15 17:02:56 +0200

    testing state space node

--- src/test/java/de/bmoth/modelchecker/ModelCheckerResultTest.java
+++ src/test/java/de/bmoth/modelchecker/ModelCheckerResultTest.java
@@ -16,6 +16,7 @@ public class ModelCheckerResultTest extends TestUsingZ3 {
     State firstState;
     State secondState;
     State thirdState;
+    State firstStateEquiv;
 
     String unknown = "check-sat ...";
 
@@ -26,14 +27,17 @@ public class ModelCheckerResultTest extends TestUsingZ3 {
         HashMap<String, Expr> firstMap = new HashMap<>();
         HashMap<String, Expr> secondMap = new HashMap<>();
         HashMap<String, Expr> thirdMap = new HashMap<>();
+        HashMap<String, Expr> firstMapEquiv = new HashMap<>();
 
         firstMap.put("x", z3Context.mkInt(10));
         secondMap.put("x", z3Context.mkInt(11));
         thirdMap.put("x", z3Context.mkInt(12));
+        firstMapEquiv.put("x", z3Context.mkInt(10));
 
         thirdState = new State(null, thirdMap);
         secondState = new State(thirdState, secondMap);
         firstState = new State(secondState, firstMap);
+        firstStateEquiv = new State(null, firstMapEquiv);
 
         StateSpaceNode thirdNode = new StateSpaceNode(thirdState);
         StateSpaceNode secondNode = new StateSpaceNode(secondState);
@@ -72,6 +76,15 @@ public class ModelCheckerResultTest extends TestUsingZ3 {
 
         assertTrue(resultNoStateSpace.getStateSpaceRoot().isEmpty());
         assertEquals("[{x=12}, successors: [{x=11}]]", resultWithStateSpace.getStateSpaceRoot().toString());
+    }
+
+    @Test
+    public void testStateSpaceNode() {
+        StateSpaceNode firstNode = new StateSpaceNode(firstState);
+
+        assertFalse(firstNode.equals(new Object()));
+        assertTrue(firstNode.equals(firstNode));
+        assertTrue(firstNode.equals(new StateSpaceNode(firstStateEquiv)));
     }
 
     @Test
